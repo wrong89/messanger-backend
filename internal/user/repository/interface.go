a73197bc72a2bd1e398e16c72fe1c0acad8188b3
wrong89 <fenomen_03@mail.ru>
2025-12-29 23:03:57 +0300

    refactoring: user repo interface; moved repo errors in separated file

--- internal/user/repository/interface.go
+++ internal/user/repository/interface.go
@@ -2,19 +2,21 @@ package repository
 
 import (
 	"context"
-	"errors"
 	"messanger/internal/user"
 )
 
-var (
-	ErrUserNotFound     = errors.New("user not found")
-	ErrUserAlreadyExist = errors.New("user already exist")
-)
-
 type UserRepo interface {
-	Create(ctx context.Context, user user.User) (uint64, error)
+	UserReader
+	UserWriter
+}
+
+type UserReader interface {
 	GetByID(ctx context.Context, id uint64) (user.User, error)
 	GetByLogin(ctx context.Context, login string) (user.User, error)
+}
+
+type UserWriter interface {
+	Create(ctx context.Context, user user.User) (uint64, error)
 	Update(ctx context.Context, user user.User) error
 	Delete(ctx context.Context, id uint64) error
 }
